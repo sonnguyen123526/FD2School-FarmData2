69cc217d9c871f803241e6741b661471d0c7b624
sonnguyen123526 <nguyenso.dickinson.edu>
2024-10-24 15:48:22 -0400

    Adding test cases to start and end date

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -7,4 +7,10 @@ describe("Test the harvest report default values", ()=>{
         cy.get("[data-cy=page-header]")
         .should("have.text","Harvest Report")
     })
+    it("Check the start and end date", ()=>{
+        cy.get("[data-cy= start-date]")
+        .type('2020-05-05').should('have.value', '2020-05-05')
+        cy.get("[data-cy= end-date]")
+        .type('2020-05-15').should('have.value', '2020-05-15')
+    })
 })
